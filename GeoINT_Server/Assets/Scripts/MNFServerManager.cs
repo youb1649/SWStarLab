1c74b684f3d3b06cff2796bb99ff85c007934cf8
youb1649 <youb1649@kaist.ac.kr>
2019-07-24 20:21:57 +0900

    Basic Server Code
    Basic Server Code

--- GeoINT_Server/Assets/Scripts/MNFServerManager.cs
+++ GeoINT_Server/Assets/Scripts/MNFServerManager.cs
@@ -1,25 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 using MNF;
 
+[System.Serializable]
+public class GeoINTServerLogEvent : UnityEvent<string>
+{
+}
+
 public class MNFServerManager : MonoBehaviour
 {
+    public GeoINTServerLogEvent thisEvent = new GeoINTServerLogEvent();
+
+    void InsertLog(string szMsg)
+    {
+
+    }
+
     void Awake()
     {
+        thisEvent.AddListener(InsertLog);
+
         LogManager.Instance.SetLogWriter(new UnityLogWriter());
         if (LogManager.Instance.Init() == false)
+        {
             Debug.Log("LogWriter init failed");
+            WriteLog("LogWriter init failed");
+        }
 
         if (TcpHelper.Instance.Start(isRunThread: false) == false)
         {
             LogManager.Instance.WriteError("TcpHelper.Instance.run() failed");
+            WriteLog("TcpHelper.Instance.run() failed");
             return;
         }
 
         if (TcpHelper.Instance.StartAccept<MNFServerManagerSession, MNFServerManagerMessageDispatcher>("10000", 500) == false)
         {
             LogManager.Instance.WriteError("TcpHelper.Instance.StartAccept<MNFServerManagerSession, MNFServerManagerMessageDispatcher>() failed");
+            WriteLog("TcpHelper.Instance.StartAccept<MNFServerManagerSession, MNFServerManagerMessageDispatcher>() failed");
             return;
         }
     }
@@ -48,5 +68,11 @@ public class MNFServerManager : MonoBehaviour
         Debug.Log("Application ending after " + Time.time + " seconds");
         TcpHelper.Instance.Stop();
         LogManager.Instance.Release();
+    }
+
+    void WriteLog(string szMsg)
+    {
+        if (null != thisEvent)
+            thisEvent.Invoke(szMsg);
     }
 }
